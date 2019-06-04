01b8ecc4c907622857d0ce5e59de2a326ff97b18
Ariful Islam <ariful@portonics.com>
2019-06-04 18:17:26 +0600

    home module

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { QuoteService } from './quote.service';
+
+@NgModule({
+  imports: [CommonModule, HomeRoutingModule],
+  declarations: [HomeComponent],
+  providers: [QuoteService]
+})
+export class HomeModule { }
